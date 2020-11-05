db980b5c27baffdd4545336f553d9d555afd1759
tmchandler <chandlertom71@gmail.com>
2020-11-05 13:33:47 -0500

    commented out the extra getOneDonut query we used for sanitization example

--- queries.js
+++ queries.js
@@ -46,15 +46,15 @@ const getOneDonut = (donutID, callback) => {
   });
 };
 
-const getOneDonut = (donutID, callback) => {
-  db.query('INSERT INTO donuts (donut_name, price) VALUES ( ? ,  ? );', [donutName, donutPrice], (err, results) => {
-    if (err) {
-      callback(err);
-    } else {
-      callback(null, results);
-    }
-  });
-};
+// const getOneDonut = (donutID, callback) => {
+//   db.query('INSERT INTO donuts (donut_name, price) VALUES ( ? ,  ? );', [donutName, donutPrice], (err, results) => {
+//     if (err) {
+//       callback(err);
+//     } else {
+//       callback(null, results);
+//     }
+//   });
+// };
 
 
 module.exports = {
